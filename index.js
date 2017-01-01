e46f5a533e7f0a35e977f92bfcf5945aba5a266c
Jussi Kinnunen <jussikin@iki.fi>
2017-01-01 18:29:32 +0200

    First version that blinks leds depeding on incoming data

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-// Import the interface to Tessel hardware
+var mqtt = require('mqtt')
 var tessel = require('tessel');
-
+var config = require('./config.json')
+var client  = mqtt.connect(config.serverAddress)
 // Turn one of the LEDs on to start.
 tessel.led[2].on();
 
+var blinkTimes = 20;
+
 // Blink!
 setInterval(function () {
-  tessel.led[2].toggle();
-  tessel.led[3].toggle();
+  if(blinkTimes>0){
+    tessel.led[2].toggle();
+    tessel.led[3].toggle();
+    blinkTimes--;
+  }
 }, 100);
 
-console.log("I'm blinking! (Press CTRL + C to stop)");
+client.on('connect', function () {
+  client.subscribe('doorBell')
+})
+
+client.on('message', function (topic, message) {
+  console.log(message.toString())
+  blinkTimes = blinkTimes+Number(message);
+})
