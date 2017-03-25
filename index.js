b36027c8f577332b5c609a7589bc58baf43a27d9
Jussi Kinnunen <jussikin@iki.fi>
2017-03-25 15:57:55 +0200

    first installed version. works as intented

--- index.js
+++ index.js
@@ -5,16 +5,52 @@ var client  = mqtt.connect(config.serverAddress)
 // Turn one of the LEDs on to start.
 tessel.led[2].on();
 
+var pin = tessel.port.A.pin[2];
+var pin2 = tessel.port.A.pin[3];
+
 var blinkTimes = 20;
+
+function standBy(){
+  pin.write(0, (error, buffer) => {
+    if (error) {
+      throw error;
+    }
+  });
+  pin2.write(0, (error, buffer) => {
+    if (error) {
+      throw error;
+    }
+  });
+}
+
+function runOn(){
+  pin.write(0, (error, buffer) => {
+    if (error) {
+      throw error;
+    }
+  });
+  pin2.write(1, (error, buffer) => {
+    if (error) {
+      throw error;
+    }
+  });
+}
+standBy();
+
 
 // Blink!
 setInterval(function () {
   if(blinkTimes>0){
     tessel.led[2].toggle();
     tessel.led[3].toggle();
-    blinkTimes--;
-  }
-}, 100);
+    pin.toggle();
+    pin2.toggle();
+  }else return
+  blinkTimes--;
+  if(blinkTimes==0)
+    standBy();
+  console.log(blinkTimes)
+}, 200);
 
 client.on('connect', function () {
   client.subscribe('doorBell')
@@ -22,5 +58,7 @@ client.on('connect', function () {
 
 client.on('message', function (topic, message) {
   console.log(message.toString())
+  runOn();
   blinkTimes = blinkTimes+Number(message);
+
 })
